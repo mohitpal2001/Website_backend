0ae4cefecc422b88683f7c63e7eab739e0763c6c
mohitpal2001 <91897271+mohitpal2001@users.noreply.github.com>
2025-11-04 18:09:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+mongoose.connect('mongodb+srv://mohit:pal2001@cluster0.yst7ouy.mongodb.net/').then(()=>{console.log("Connected to MongoDB")}
+).catch((err)=>console.log(err));
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+    origin:"http://localhost:5173/",
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-Type","Authorization","Cache-Control","Expires","pragma"],
+    credentials:true
+}))
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
