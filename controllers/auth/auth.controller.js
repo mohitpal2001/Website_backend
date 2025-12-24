26fb41b706d469a8c4abf8df1908a149224d0f98
mohitpal2001 <91897271+mohitpal2001@users.noreply.github.com>
2025-12-24 16:54:34 +0530

    changes in login api

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -58,7 +58,7 @@ const loginUser = async(req,res)=>{
    res.cookie('token', token, {
   httpOnly: true,
   secure: false,      
-  sameSite: "lax" 
+  sameSite: "none" 
 }).json({
             message:'Logged in succesfully',
         user:{
